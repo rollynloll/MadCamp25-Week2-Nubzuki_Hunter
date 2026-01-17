8fefbe3a4ca3112d17cf3ee89c85449a8778085a
keumchaewon <sskeum2009@naver.com>
2026-01-17 16:03:46 +0900

    fix: 이미지 깨짐 문제 해결

--- Frontend/src/ui/auth/LoginCard.jsx
+++ Frontend/src/ui/auth/LoginCard.jsx
@@ -1,9 +1,11 @@
 // src/ui/auth/LoginCard.jsx
+import nubzukiImg from "../../assets/images/nubzuki.png";
+
 export default function LoginCard({ title, subtitle, children }) {
   return (
     <div className="login-card">
       <img
-        src="/assets/images/nubzuki.png"
+        src={nubzukiImg}
         alt="nubzuki"
         className="login-character"
       />
